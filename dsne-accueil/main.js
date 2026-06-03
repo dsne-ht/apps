d8f0525f01795fe907aa90f892d66ce7a3bb2cf5
dsne-ht <sec.direction.dsne@gmail.com>
2026-06-03 00:22:41 -0400

    Add Daisha admin code to accueil

--- dsne-accueil/main.js
+++ dsne-accueil/main.js
@@ -84,6 +84,7 @@ function initDB() {
 
     -- Réceptionnistes préchargées
     INSERT OR IGNORE INTO users (code, nom_complet, email, role) VALUES
+      ('174839', 'Daisha Dorsainvil', 'sec.direction.dsne@gmail.com', 'admin'),
       ('145056', 'Geralda Michel', 'reception.dsne@gmail.com', 'reception'),
       ('582005', 'Renande Destiné', 'reception.dsne@gmail.com', 'reception'),
       ('789043', 'Vasna Pierre', 'reception.dsne@gmail.com', 'reception'),
